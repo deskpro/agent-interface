58f7d8089ed0674910adeb39f125bd65004ff2e5
Artem Berdyshev <berdartem@gmail.com>
2018-12-06 18:16:49 +0200

    add buttons demo

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,18 +1,46 @@
 import * as React from "react";
+import * as PropTypes from "prop-types";
+import classNames from "classnames";
 import "./Button.css";
 
+const colors = ["primary", "secondary", "success", "info", "warning", "danger"];
+const sizes = ["small", "medium", "large"];
+
 export type ButtonProps = {
+  size?: string;
+  color?: string;
+  rounded?: boolean;
   onClick(e: React.MouseEvent<HTMLElement>): void;
 };
 
 const Button: React.SFC<ButtonProps> = ({
   onClick: handleClick,
   children,
+  size = "",
+  color = "",
+  rounded = false,
   ...props
 }) => (
-  <button type="button" className="btn" onClick={handleClick} {...props}>
+  <button
+    type="button"
+    className={classNames("dp-Button", {
+      [`Button--${color}`]: colors.includes(color),
+      [`Button--${size}`]: sizes.includes(size),
+      "Button--rounded": rounded
+    })}
+    onClick={handleClick}
+    {...props}
+  >
     {children}
   </button>
 );
+
+// @ts-ignore
+// Button.propTypes = {
+//   size: PropTypes.oneOf(sizes),
+//   color: PropTypes.oneOf(colors),
+//   onClick: PropTypes.func.isRequired,
+//   rounded: PropTypes.bool
+// };
 
 export default Button;
