d4fa6eb313fc853b82334424bf8dc307408730b9
Artem Berdyshev <berdartem@gmail.com>
2018-12-12 15:47:04 +0200

    update dp-buttons.css file name

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import classNames from "classnames";
-import "@deskpro/agent-interface-style/dist/elements/buttons.css";
+import "@deskpro/agent-interface-style/dist/elements/dp-buttons.css";
 
 export interface ButtonProps {
   size?: "default" | "small";
