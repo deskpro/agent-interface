1a728cdd4bbd557996bfe57cb1f9a12c9a31e5f4
Artem Berdyshev <berdartem@gmail.com>
2018-12-12 14:39:15 +0200

    update button components

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -27,9 +27,7 @@ const Button: React.SFC<ButtonProps> = ({
     type="button"
     className={classNames("dp-Button", {
       [`Button--${size}`]: !!size && size !== "default",
-      [`Button--${variant}`]: !!variant && variant !== "contained",
-      "dp-ActionButton": variant === "actions",
-      "dp-ExpandButton": variant === "more"
+      [`Button--${variant}`]: !!variant && variant !== "contained"
     })}
     onClick={handleClick}
     {...props}
