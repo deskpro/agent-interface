1d1107c0f199ab00cae53af555619a8c19ac0ea3
Artem Berdyshev <berdyshev@Artems-MacBook-Pro.local>
2019-01-30 14:22:10 +0200

    refactor standard example for kanban

--- packages/components-demo/stories/components/StandardKanbanDemo.jsx
+++ packages/components-demo/stories/components/StandardKanbanDemo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import produce from "immer";
 
 import {
   StandardKanban,
@@ -7,58 +8,70 @@ import {
   UserInfo,
   Menu
 } from "@deskpro/agent-interface-components";
-import produce from "immer";
 
 const columns = ["Backlog", "Started", "In Progress", "Review", "QA", "Closed"];
 
-function useKanbanDragging() {
-  const [data, setData] = React.useState(
-    columns.reduce((acc, column, columnIndex) => {
-      acc.push({
-        group: { id: column, title: column },
-        items: Array.from(
-          { length: Math.round(Math.random() * 20) },
-          (_, idx) => ({
-            id: `${columnIndex}${idx}`,
-            title: `Card #${columnIndex}${idx + 1}`
-          })
-        )
-      });
-      return acc;
-    }, [])
-  );
+const kanbanReducer = produce((draft, { type, payload }) => {
+  switch (type) {
+    case "loadData":
+      return columns.reduce((acc, column, columnIndex) => {
+        acc.push({
+          group: { id: column, title: column },
+          items: Array.from(
+            { length: Math.round(Math.random() * 20) },
+            (_, idx) => ({
+              id: `${columnIndex}${idx}`,
+              title: `Card #${columnIndex}${idx + 1}`
+            })
+          )
+        });
+        return acc;
+      }, []);
 
-  const onDragEnd = React.useCallback(
-    ({ itemId, fromGroupId, toGroupId, index }) => {
-      const nextData = produce(data, draft => {
-        const sourceDataItem = draft.find(i => i.group.id === fromGroupId);
-        const [moved] = sourceDataItem.items.splice(
-          sourceDataItem.items.findIndex(i => i.id === itemId),
-          1
-        );
-        const destDataItem = draft.find(i => i.group.id === toGroupId);
-        destDataItem.items.splice(index, 0, moved);
-      });
-      setData(nextData);
-    },
-    [data]
-  );
+    case "moveItem": {
+      const { itemId, fromGroupId, toGroupId, index } = payload;
+      const sourceDataItem = draft.find(i => i.group.id === fromGroupId);
+      const [moved] = sourceDataItem.items.splice(
+        sourceDataItem.items.findIndex(i => i.id === itemId),
+        1
+      );
+      const destDataItem = draft.find(i => i.group.id === toGroupId);
+      destDataItem.items.splice(index, 0, moved);
+      break;
+    }
+    case "loadMore": {
+      const item = draft.find(i => i.group.id === payload.id);
+      if (item) {
+        const columnIdx = columns.indexOf(payload.title);
+        const itemsCount = item.items.length;
+        for (let i = 0; i < 10; i += 1) {
+          item.items.push({
+            id: `${columnIdx}${itemsCount + i}`,
+            title: `Card #${columnIdx}${itemsCount + i + 1}`
+          });
+        }
+      }
+      break;
+    }
 
-  return { data, onDragEnd };
-}
+    default:
+      break;
+  }
+  return undefined;
+});
 
 const StandardKanbanDemo = ({ action }) => {
-  const { data, onDragEnd } = useKanbanDragging();
+  const [data, dispatch] = React.useReducer(kanbanReducer, [], {
+    type: "loadData"
+  });
 
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <StandardKanban
         className="Kanban--cards"
         data={data}
-        onDragEnd={onDragEnd}
-        onLoadMore={group =>
-          action(`scroll reached threshold for group ${group.title}`)()
-        }
+        onDragEnd={payload => dispatch({ type: "moveItem", payload })}
+        onLoadMore={group => dispatch({ type: "loadMore", payload: group })}
         renderCard={card => (
           <Card.TicketCard
             model={card}
