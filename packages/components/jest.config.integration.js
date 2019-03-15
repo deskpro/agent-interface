42bb661eb536ef67a8b52ea78ef5ba89526d8676
Julien Ducro <julien.ducro@deskpro.com>
2019-03-15 10:59:51 +0100

    Add codecov integration

--- packages/components/jest.config.integration.js
+++ packages/components/jest.config.integration.js
@@ -6,5 +6,6 @@ config.testMatch = [
   join(__dirname, "src/**/*.integration.test.{js,jsx,ts,tsx}")
 ];
 config.setupTestFrameworkScriptFile = "./jest.setup.integration.js";
+config.coverageDirectory = "coverage/integration";
 console.log("RUNNING INTEGRATION TESTS");
 module.exports = config;
--- packages/components/jest.config.unit.js
+++ packages/components/jest.config.unit.js
@@ -3,5 +3,6 @@ const { join } = require("path");
 const config = require("./jest.config");
 
 config.testMatch = [join(__dirname, "src/**/*.unit.test.{js,jsx,ts,tsx}")];
+config.coverageDirectory = "coverage/unit";
 console.log("RUNNING UNIT TESTS");
 module.exports = config;
