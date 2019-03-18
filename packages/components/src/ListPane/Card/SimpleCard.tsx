20ac2cb443d999a354548295922f3142f7aab799
Artem Berdyshev <berdartem@gmail.com>
2019-03-18 12:23:24 +0200

    remove extra dp-Level for Card SimpleCard component

--- packages/components/src/ListPane/Card/SimpleCard.tsx
+++ packages/components/src/ListPane/Card/SimpleCard.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import classNames from "classnames";
 
 import Card, { BasicCardProps } from "./Card";
 import Checkbox, { CheckableCardProps } from "./CardCheckbox";
@@ -35,11 +34,7 @@ const SimpleCard: React.FC<SimpleCardProps> = ({
   onActionClick,
   ...props
 }) => (
-  <Card
-    cardId={cardId}
-    {...props}
-    className={classNames("dp-Level", className)}
-  >
+  <Card cardId={cardId} {...props} className={className}>
     {checkable && (
       <span className="dp-LevelItem dp-LevelCol dp-LevelTop">
         {!!checkable && (
