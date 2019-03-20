9b0b96f9d4baa4517bcc399efaf45b09598c8509
Julien Ducro <julien.ducro@deskpro.com>
2019-03-20 14:08:15 +0100

    Add ModuleBar tests

---
+++ packages/components/src/elements/ModuleBar/__tests__/ModuleBar.unit.test.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { render } from "react-testing-library";
+
+import ModuleBar from "../ModuleBar";
+import ModuleBarMenu from "../ModuleBarMenu";
+
+describe("<TagBar />", () => {
+  it("it should match its snapshot", () => {
+    const handleClick = jest.fn();
+    const { container } = render(
+      <ModuleBar className="module-bar">
+        <ModuleBar.Item
+          icon="ticket"
+          label="Tickets"
+          isActive
+          onClick={handleClick}
+        />
+        <ModuleBar.Item
+          icon="billing"
+          label="Billing"
+          isActive={false}
+          onClick={handleClick}
+        />
+        <ModuleBar.Separator />
+        <ModuleBar.Item icon="portal" onClick={handleClick}>
+          <ModuleBarMenu title="Portals" isVisible>
+            <ModuleBarMenu.Item
+              icon="plane"
+              title="Raven Paper"
+              subtitle="support.ravenpaper.com"
+              link="http://support.ravenpaper.com"
+            />
+            <ModuleBarMenu.Item
+              icon="plane-violet"
+              title="RP Europe"
+              subtitle="support.rpeurope.com"
+              link="http://support.rpeurope.com"
+            />
+          </ModuleBarMenu>
+        </ModuleBar.Item>
+      </ModuleBar>
+    );
+    expect(container.querySelector(".module-bar")).toMatchSnapshot();
+  });
+});
