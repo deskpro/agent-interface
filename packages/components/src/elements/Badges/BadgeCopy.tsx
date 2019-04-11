3ec09a3ecc906d897ffa012f7596eb8a4f7a7710
Artem Berdyshev <berdartem@gmail.com>
2019-04-11 15:12:15 +0300

    update markup for the BadgeCopy component

--- packages/components/src/elements/Badges/BadgeCopy.tsx
+++ packages/components/src/elements/Badges/BadgeCopy.tsx
@@ -18,34 +18,36 @@ const BadgeCopy: React.FC<BadgeCopyProps> = ({ value, className }) => {
 
   return (
     <Manager>
-      <span className="dp-BadgeWrapper">
-        <Reference>
-          {({ ref }) => (
-            <Badge
-              type="id"
-              className={classNames(className, { "is-active": isOpen })}
-              label={`#${value}`}
-              onClick={() => setOpenState(!isOpen)}
-              ref={ref}
-            />
-          )}
-        </Reference>
-        {isOpen && (
-          <Popper
-            placement="right"
-            modifiers={{
-              offset: { offset: "0,5" }
-            }}
-          >
-            {({ ref, style }) => (
-              <span className="dp-ToolTips" ref={ref} style={style}>
-                <CopyToClipboard value={value} />
-                <Icon name="link" size={17} />
-              </span>
-            )}
-          </Popper>
+      <Reference>
+        {({ ref }) => (
+          <Badge
+            type="id"
+            className={classNames(className, { "is-active": isOpen })}
+            label={`#${value}`}
+            onClick={() => setOpenState(!isOpen)}
+            ref={ref}
+          />
         )}
-      </span>
+      </Reference>
+      {isOpen && (
+        <Popper
+          placement="right"
+          modifiers={{
+            offset: { offset: "0,5" }
+          }}
+        >
+          {({ ref, style }) => (
+            <span
+              className="dp-ToolTips ToolTips--badge"
+              ref={ref}
+              style={style}
+            >
+              <CopyToClipboard value={value} />
+              <Icon name="link" size={17} />
+            </span>
+          )}
+        </Popper>
+      )}
     </Manager>
   );
 };
